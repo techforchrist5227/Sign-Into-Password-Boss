2423a0cfcc563710004859e11f59dd26c832e0f6
Brett Raper <72267696+techforchrist5227@users.noreply.github.com>
2022-11-28 11:23:02 -0500

    Uploaded password Boss auto sign in

---
+++ passwordbosspasswordenter (1).py
@@ -0,0 +1,9 @@
+import os
+from pywinauto.application import Application
+from pywinauto import Desktop
+from pywinauto.keyboard import send_keys
+app = Application(backend="uia").start(r"c:\Users\BrettRaper\AppData\Local\PasswordBoss\PasswordBoss.exe").connect(title="Password Boss", timeout=20)
+
+passwordEnter = app.PasswordBoss.child_window(auto_id="GlobalPasswordTextBox", control_type="Edit").wrapper_object()
+
+passwordEnter.type_keys("Laursmolomarpippin5227!!{ENTER}")
