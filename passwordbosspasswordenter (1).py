274d7a0f07f729dddaecd9ffa4a60d0003b1120e
Brett Raper <72267696+techforchrist5227@users.noreply.github.com>
2023-04-20 17:07:34 -0400

    Updated Timeout and REMEMBER SOMTEHING
    Updated timeout
    
    password isn't included for security purposes.

--- passwordbosspasswordenter (1).py
+++ passwordbosspasswordenter (1).py
@@ -2,7 +2,7 @@ import os
 from pywinauto.application import Application
 from pywinauto import Desktop
 from pywinauto.keyboard import send_keys
-app = Application(backend="uia").start(r"c:\Users\BrettRaper\AppData\Local\PasswordBoss\PasswordBoss.exe").connect(title="Password Boss", timeout=20)
+app = Application(backend="uia").start(r"c:\Users\BrettRaper\AppData\Local\PasswordBoss\PasswordBoss.exe").connect(title="Password Boss", timeout=200000000)
 
 passwordEnter = app.PasswordBoss.child_window(auto_id="GlobalPasswordTextBox", control_type="Edit").wrapper_object()
 
